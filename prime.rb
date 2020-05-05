7d747fef6189e0e08a07922d543b8291bfab779d
Bobby Neal <rsneal01@gmail.com>
2020-05-05 16:27:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,14 @@
 # Add  code here!
 
-# def prime?(num)
-#   (2..(num - 1)).each do |n|
-#     return false if num % n == 0
-#   end
-#   true
-# end
+def prime?(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
+end
 
 # def prime?(num)
-# (2..(num - 1)).to_a.each do |n|
+#   (2..(num - 1)).to_a.each do |n|
 #     if num % n == 0
 #       false
 #     else 
