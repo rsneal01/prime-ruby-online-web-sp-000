a808b577e526bb8c69b533d30192eda86327439b
Bobby Neal <rsneal01@gmail.com>
2020-05-05 17:02:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+
+# def prime?(num)
+#   (2..(num - 1)).each do |n|
+#     return false if num % n == 0
+#   end
+#   true
+# end
+
+def prime?(num)
+  if num <= 1
+    false
+  else
+  (2..(num - 1)).each do |n|
+    if num % n == 0
+      return false
+      # explicitly returning false breaks us out of the loop, so no else statement
+    end
+  end
+      true
+  end
+end
